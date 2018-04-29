598e1b560dddcb78942f90de9d3dd63bae80050c
coovole <31720831+coovole@users.noreply.github.com>
2018-04-28 22:55:23 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AmaryllisBot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public int HandleCommandAsync { get; private set; }
+
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            string botToken = "NDM5NjU2NzU5Mjk3MTc5NjYz.DcWWRQ.NQrAasoJBfYpzJSEmlB4GX6NgfY";
+
+            //event subscriptions
+            _client.Log += Log;
+            _client.UserJoined += Join;
+            _client.UserLeft += Leave;
+
+            //Context.User
+            //Context.Client
+            //Context.Guild
+            //Context.Message
+            //Context.Channel
+
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, botToken);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+
+        }
+
+        private async Task Leave(SocketGuildUser arg)
+        {
+            var guild = arg.Guild;
+            var channel = guild.DefaultChannel;
+            await channel.SendMessageAsync($"{arg.Username} has left {guild.Name} :sob:");
+        }
+
+        private async Task Join(SocketGuildUser user)
+        {
+            var guild = user.Guild;
+            var channel = guild.DefaultChannel;
+            await channel.SendMessageAsync($"Welcome, {user.Mention} to {guild.Name}");
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandlecommandAsync;
+
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandlecommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            if (message is null || message.Author.IsBot) return;
+
+            int argPos = 0;
+
+            if (message.HasStringPrefix(">", ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
+                var context = new SocketCommandContext(_client, message);
+
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess)
+                    Console.WriteLine(result.ErrorReason);
+            }
+        }
+    }
+}
